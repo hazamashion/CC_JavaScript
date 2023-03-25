12b7a834064b79d5bebdd1f364386c41df702ff2
hazamashion <shionn1202@gmail.com>
2023-03-25 19:12:52 +0900

    不要なディレクトリを除外

---
+++ js/11/javascript/cockcamp.js
@@ -0,0 +1,24 @@
+$('.carousel').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+});
+$('.new_recipe').modaal();
+
+$('#share').click(() => {
+    alert('ダイエットのレシピをシェアしました。');
+    $('.new_recipe').modaal('close');
+});
+$('#share_2').click(() => {
+    alert('ダイエットのレシピをシェアしました。');
+    $('.new_recipe').modaal('close');
+});
+$('#share_3').click(() => {
+    alert('ダイエットのレシピをシェアしました。');
+    $('.new_recipe').modaal('close');
+});
+$('#share_4').click(() => {
+    alert('ダイエットのレシピをシェアしました。');
+    $('.new_recipe').modaal('close');
+});
